3c51cbb2405808d01b9819edcd43b1393b380ce4
eyener3 <ayana3330@icloud.com>
2019-06-10 02:08:44 +0900

    “X” 側と “O” 側が交互に着手できる実装

--- src/index.js
+++ src/index.js
@@ -15,13 +15,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xISNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    squares[i] = this.state.xISNext ? 'X' : '0';
+    this.setState({
+      squares: squares,
+      xISNext: !this.state.xISNext,
+    });
   }
 
   renderSquare(i) {
